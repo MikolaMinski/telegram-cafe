6f05f80d973950e9e5bfedd6e42c7addd34bc265
Mikola <Kolianych89@mail.ru>
2025-05-26 16:23:04 +0300

    1

--- src/Order.tsx
+++ src/Order.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useCart } from './CartContext';
 
-const API_URL = 'http://cafevozeraapi.somee.com/api/Message/SendPost'; // замените на ваш API
+const API_URL = 'https://cafevozeraapi.somee.com/api/Message/SendPost'; // замените на ваш API
 
 const Order = () => {
   const { cart, clearCart } = useCart();
