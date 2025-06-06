d51953a17b8bc3702f4977e97fda0e08ec945104
Mikola <Kolianych89@mail.ru>
2025-06-06 12:43:02 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     if (window.Telegram && window.Telegram.WebApp) {
       window.Telegram.WebApp.ready();
-      const tgUser = window.Telegram.WebApp.initDataUnsafe?.user || null;
+      const tgUser = window.Telegram || null;
       setUser(tgUser);
       window.Telegram.WebApp.expand();
     }
